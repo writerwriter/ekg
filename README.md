eccd86fb81e940447a004e15bc1f82cece57ec7f
toosyou <toosyou.tw@gmail.com>
2020-03-21 04:28:59 +0800

    refactor the big_exam data generation for uniting with audicor 10s data

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ekg
+
+## Before Experiments
+1. Adjust `config.cfg`.
+1. Run `python3 scripts/data_preprocessing/big_exam.py` to generate data to `data` directory
--- ekg/utils/data_utils.py
+++ ekg/utils/data_utils.py
@@ -2,24 +2,10 @@ import numpy as np
 from functools import partial
 import multiprocessing as mp
 from tqdm import tqdm
-import pandas as pd
 from sklearn.model_selection import train_test_split
 import better_exceptions; better_exceptions.hook()
-import os
-import configparser
-from functools import partial
 
 from ..audicor_reader import reader
-
-config = configparser.ConfigParser()
-config.read(os.path.join(os.path.dirname(__file__), '..', 'configs.ini'))
-OUTCOME_FILENAME = config['Paths']['outcome']
-LVEF_PATH_FILENAME = config['Paths']['lvef_path']
-PATH_DUR_FILENAME = config['Paths']['path_dur']
-DIRTYDATA_INDICES_FILENAME = config['Paths']['dirtydata_indices']
-
-BIG_EXAM_DIR = config['Paths']['big_exam_dir']
-BIG_EXAM_NORMAL_DIRS = config['Paths']['big_exam_normal_dirs'].split(', ')
 
 NUM_PROCESSES = mp.cpu_count()*2
 
@@ -81,79 +67,6 @@ def to_spectrogram(data):
 
     return np.array(rtn)
 
-def mp_get_ekg(filenames, do_bandpass_filter=True, filter_lowcut=30, filter_highcut=100, desc='Loading data'):
-    X = list()
-    __mp_get_ekg = partial(reader.get_ekg, do_bandpass_filter=do_bandpass_filter, filter_lowcut=filter_lowcut, filter_highcut=filter_highcut)
-    with mp.Pool(processes=NUM_PROCESSES) as workers:
-        for xi, _ in tqdm(workers.imap(__mp_get_ekg, filenames), total=len(filenames), desc=desc):
-            X.append(xi)
-
-    return np.array(X)
-
-def load_normal_data(do_bandpass_filter=True, filter_lowcut=30, filter_highcut=100):
-    def get_normal_filenames(dirname):
-        filenames = list()
-        for directory, sub_directory, filelist in os.walk(dirname, topdown=True):
-            for fn in filelist:
-                if fn.endswith('.bin'):
-                    filenames.append(os.path.join(directory, fn))
-        return filenames
-
-    filenames = list()
-    for dirname in BIG_EXAM_NORMAL_DIRS:
-        filenames = filenames + get_normal_filenames(dirname)
-    return mp_get_ekg(filenames, do_bandpass_filter, filter_lowcut, filter_highcut, 'Loading normal data')
-
-def path_is_dirty(pi, dirtydata_indices_df, remove_dirty):
-    for dp in dirtydata_indices_df['dirty']:
-        if dp == dp and dp in pi: return True
-    if remove_dirty == 2: # not-so-dirty data
-        for dp in dirtydata_indices_df['gray_zone']:
-            if dp == dp and dp in pi: return True
-    return False
-
-def load_patient_data(do_bandpass_filter=True, filter_lowcut=30, filter_highcut=100, remove_dirty=0):
-    '''
-        remove_dirty:
-            0 - don't remove dirty data
-            1 - remove dirty data
-            2 - remove dirty data and not-so-dirty data
-    '''
-    path_lvef_df = pd.read_csv(LVEF_PATH_FILENAME, header=None, names=['path', 'LVEF'])
-    paths = path_lvef_df['path'].values
-    # clean up
-    if remove_dirty > 0:
-        dirtydata_indices_df = pd.read_excel(DIRTYDATA_INDICES_FILENAME)
-        paths = np.array([p for p in paths if not path_is_dirty(p, dirtydata_indices_df, remove_dirty)])
-
-    filenames = list(map(lambda x: BIG_EXAM_DIR+x, paths))
-    patient_id = np.array([p.split('/')[1] for p in paths])
-    X = mp_get_ekg(filenames, do_bandpass_filter, filter_lowcut, filter_highcut, 'Loading patient data')
-
-    return X, patient_id
-
-def load_patient_LVEF():
-    path_lvef_df = pd.read_csv(LVEF_PATH_FILENAME, header=None, names=['path', 'LVEF'])
-    return path_lvef_df['LVEF'].values
-
-def load_target(target_name, dtype=int):
-    ahf2017_df = pd.read_excel(OUTCOME_FILENAME, skiprows=1)
-    path_lvef_df = pd.read_csv(LVEF_PATH_FILENAME, header=None, names=['path', 'LVEF'])
-
-    # get patient code
-    patient_id = list()
-    for p in path_lvef_df['path']:
-        patient_id.append(p.split('/')[1])
-
-    target = list()
-    for p in patient_id:
-        try:
-            target.append(dtype(ahf2017_df[target_name][ahf2017_df.code == p]))
-        except:
-            target.append(0)
-
-    return np.array(target)
-
 def patient_split(X, y, patient_id, rs=42):
     unique_patient_id = np.unique(np.array(patient_id))
 
@@ -171,41 +84,5 @@ def patient_split(X, y, patient_id, rs=42):
 
     return [[X_train, y_train], [X_valid, y_valid], [X_test, y_test]]
 
-def load_survival_data(event_name, remove_dirty=0):
-    '''
-        remove_dirty:
-            0 - don't remove dirty data
-            1 - remove dirty data
-            2 - remove dirty data and not-so-dirty data
-    '''
-    dur_df = pd.read_csv(PATH_DUR_FILENAME)
-
-    if remove_dirty > 0:
-        dirtydata_indices_df = pd.read_excel(DIRTYDATA_INDICES_FILENAME)
-        filter_function = lambda row: not path_is_dirty(row['path'], dirtydata_indices_df, remove_dirty)
-        dur_df = dur_df[dur_df.apply(filter_function, axis=1)].reset_index(drop=True)
-
-    # dur_df.follow_dur[i] < 0: this measurement is done after the final follow date
-    # dur_df.ADHF_dur[i] != dur_df.ADHF_dur[i]: no ADHF occurred, survived
-    # dur_df.ADHF_dur[i] < 0: this measurement is done after event
-
-    event_dur_name = event_name + '_dur'
-
-    # 1: event occurred, 0: survived, -1: pass event, ignore
-    dur_df['censoring_stats'] = 1 # event occurred
-    dur_df.loc[dur_df[event_dur_name] != dur_df[event_dur_name], 'censoring_stats'] = 0 # Nan
-    dur_df.loc[(dur_df.follow_dur <= 0) | (dur_df[event_dur_name] < 0) | (dur_df.path.str.contains('NG')), 'censoring_stats'] = -1
-
-    dur_df['survival_times'] = dur_df[event_dur_name] # event occurred
-    dur_df.loc[dur_df[event_dur_name] != dur_df[event_dur_name], 'survival_times'] = dur_df.follow_dur[dur_df[event_dur_name] != dur_df[event_dur_name]] # Nan
-    dur_df.loc[(dur_df.follow_dur <= 0) | (dur_df[event_dur_name] < 0) | (dur_df.path.str.contains('NG')), 'survival_times'] = 0
-
-    censoring_stats = dur_df['censoring_stats'].values
-    survival_times = dur_df['survival_times'].values.astype(int)
-
-    return censoring_stats, survival_times
-
 if __name__ == '__main__':
-    print(load_survival_data('ADHF', 2))
-    # patient_X, patient_id = load_patient_data(remove_dirty=2)
-    # normal_X = load_normal_data()
+    pass
